15b330c6d296b9566459b6ce6e229e5797096499
marshall2023 <74441675+michaelfan0310@users.noreply.github.com>
2022-06-25 18:55:53 -0400

    2nd commit

--- src/main.js
+++ src/main.js
@@ -4,5 +4,10 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el:'#app',
+	render: h => h(App),
+  
+	// beforeCreate() {
+	// 	Vue.prototype.$bus = this
+	// },
+})
